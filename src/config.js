5fc8dfbbb53b91e5bf6fcc331c238cf1f265f287
Mostafa10Oulahyan <mostaphaoulahyan@gmail.com>
2026-02-27 12:27:41 +0000

    Update API URLs after redeployment

--- src/config.js
+++ src/config.js
@@ -2,7 +2,7 @@
 // Using Vercel deployed URLs
 
 export const API_CONFIG = {
-  BOOKS_API: "https://tp5-nodejs-rest-api-microservice-li.vercel.app",
-  CUSTOMERS_API: "https://tp5-nodejs-rest-api-microservice-li-eight.vercel.app",
-  ORDERS_API: "https://tp5-nodejs-rest-api-microservice-li-olive.vercel.app",
+  BOOKS_API: "https://tp5-nodejs-rest-api-microservice-li-nine.vercel.app",
+  CUSTOMERS_API: "https://tp5-nodejs-rest-api-microservice-li.vercel.app",
+  ORDERS_API: "https://tp5-nodejs-rest-api-microservice-li-ten.vercel.app",
 };
